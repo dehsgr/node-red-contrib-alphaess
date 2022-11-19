10e11443020a3e1f0e03667f02d9aa13b6982bcb
dehsgr <dehsgr@outlook.com>
2022-11-19 17:55:08 +0100

    Switched back to hard coded signature key.

--- alphaess-monitoring.js
+++ alphaess-monitoring.js
@@ -12,7 +12,7 @@ module.exports = function(RED)
 		var Loop;
 	
 		this.Auth = undefined;
-		this.SignatureKey = undefined;
+		this.SignatureKey = "LS885ZYDA95JVFQKUIUUUV7PQNODZRDZIS4ERREDS0EED8BCWSS";	// derieved from app.XYZ.js
 		this.Serial = myNode.serial;
 		this.Username = myNode.username;
 		this.Password = myNode.password;
@@ -42,8 +42,6 @@ module.exports = function(RED)
 			var now = new Date();
 			return now.getFullYear() + '-' + (now.getMonth() + 1) + '-' + now.getDate();
 		}});
-
-		Platform.initialize();
 
 		function monitor()
 		{
@@ -95,74 +93,6 @@ module.exports = function(RED)
 		headers.AuthSignature = 'al8e4s' + hash + 'ui893ed';
 
 		return Object.assign({}, headers, myAdditionalHeaders);
-	}
-
-	AlphaESS.prototype.initialize = function ()
-	{
-		const APP_SRC = /\<script src\=(\/static\/js\/app\..*?\.js\?.*?)\>/gis;
-		const APP_KEY = /"(LS.*?CWSS)"/gis;
-
-		var Platform = this;
-
-		Platform.log('Initializing...');
-
-		require('request')({
-			gzip: true,
-			method: 'GET',
-			url: Platform.BaseURI + '../',
-		}, function(myError, myResponse) {
-			if(myError)
-			{
-				Platform.warn('There was an error during fetching Alpha ESS Application Source: ' + myError);
-			}
-			else
-			{
-				try {
-					var body = myResponse.body;
-					var js = APP_SRC.exec(body);
-					if (!js || js.length < 2) {
-						throw 'We got an unexpected body.';
-					}
-
-					js = js[1];
-
-					Platform.debug('We got the following Application Source: ' + js);
-
-					require('request')({
-						gzip: true,
-						method: 'GET',
-						url: Platform.BaseURI + '..' + js,
-					}, function(myError, myResponse) {
-						if(myError)
-						{
-							Platform.warn('There was an error during fetching Alpha ESS Signature Key: ' + myError);
-						}
-						else
-						{
-							try {
-								var body = myResponse.body;
-								var key = APP_KEY.exec(body);
-								if (!key || key.length < 2) {
-									throw 'We got an unexpected body.';
-								}
-			
-								Platform.SignatureKey = key[1];
-			
-								Platform.debug('We got the following Application Signature Key: ' + Platform.SignatureKey);
-							}
-							catch(myError)
-							{
-								Platform.warn('There was an error during fetching Alpha ESS Signature Key: ' + myError + '\r\n\r\nWe got the following body:\r\n' + myResponse.body);
-							}
-						}
-					});
-				}
-				catch(myError)
-				{
-					Platform.warn('There was an error during fetching Alpha ESS Application Source: ' + myError + '\r\n\r\nWe got the following body:\r\n' + myResponse.body);
-				}
-			}
-		});
 	}
 
 	AlphaESS.prototype.authenticate = function()
